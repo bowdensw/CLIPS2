c98730b3510af5c12507f5dcef6205a8d9c884d2
Spencer Bowden <spencer.w.bowden@vanderbilt.edu>
2025-06-12 10:40:02 -0500

    testing files done as well

---
+++ net2_test1dax.ex
@@ -0,0 +1,39 @@
+name: test1dax_consistent
+I:
+(left_obj)
+0 0 0.25 0.25
+(right_obj)
+0.75 1 0.75 1
+(left_name)
+1 0 0
+(right_name)
+1 0 0 
+T:
+(left_obj_OUT)
+0 0 0.25 0.25
+(right_obj_OUT)
+0.75 1 0.75 1
+(left_name_OUT)
+1 0 0
+(right_name_OUT)
+1 0 0 
+;
+name: test1dax_inconsistent
+I:
+(left_obj)
+0 0 0.25 0.25
+(right_obj)
+0.25 0 0.25 0
+(left_name)
+1 0 0 
+(right_name)
+0 1 0
+T:
+(left_obj_OUT)
+0 0 0.25 0.25
+(right_obj_OUT)
+0.25 0 0.25 0
+(left_name_OUT)
+1 0 0 
+(right_name_OUT)
+0 1 0
