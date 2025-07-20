9635360e2a2dfbc366f91a32d2f3d5be16d4a26d
Spencer Bowden <spencer.w.bowden@vanderbilt.edu>
2025-07-20 18:02:56 -0500

    third network general struct

---
+++ network_3/net3_test2dax.ex
@@ -0,0 +1,24 @@
+name: test2dax_consistent
+I:
+(left_obj)
+1 1 0.75 0.75
+(right_obj)
+0.25 0 0.25 0
+T:
+(left_name_OUT)
+1 0 0
+(right_name_OUT)
+1 0 0 
+;
+name: test2dax_inconsistent
+I:
+(left_obj)
+1 1 0.75 0.75
+(right_obj)
+0.75 1 0.75 1
+T:
+(left_name_OUT)
+1 0 0 
+(right_name_OUT)
+0 1 0
+;
---
+++ network_3/net3_train2_fip.ex
@@ -0,0 +1,84 @@
+
+name: train2_fip
+8
+[0] I:
+(left_obj)
+1 1 0.75 0.75
+(right_obj)
+0 0 0.25 0.25
+T:
+(left_name_OUT)
+0 1 0
+(right_name_OUT)
+0 0 0
+[1] I:
+(left_obj)
+1 1 0.75 0.75
+(right_obj)
+0 0 0.25 0.25
+T:
+(left_name_OUT)
+0 0 0
+(right_name_OUT)
+0 1 0
+[2] I:
+(left_obj)
+1 1 0.75 0.75
+(right_obj)
+0 0.25 0 0.25
+T:
+(left_name_OUT)
+0 1 0
+(right_name_OUT)
+0 0 0
+[3] I:
+(left_obj)
+1 1 0.75 0.75
+(right_obj)
+0 0.25 0 0.25
+T:
+(left_name_OUT)
+0 0 0
+(right_name_OUT)
+0 1 0
+[4] I:
+(left_obj)
+1 1 0.75 0.75
+(right_obj)
+1 0.75 1 0.75
+T:
+(left_name_OUT)
+0 1 0
+(right_name_OUT)
+0 0 0
+[5] I:
+(left_obj)
+1 1 0.75 0.75
+(right_obj)
+1 0.75 1 0.75
+T:
+(left_name_OUT)
+0 0 0
+(right_name_OUT)
+1 0 0
+[6] I:
+(left_obj)
+1 1 0.75 0.75
+(right_obj)
+0.75 0.75 1 1
+T:
+(left_name_OUT)
+0 1 0
+(right_name_OUT)
+0 0 0
+[7] I:
+(left_obj)
+1 1 0.75 0.75
+(right_obj)
+0.75 0.75 1 1
+T:
+(left_name_OUT)
+0 0 0
+(right_name_OUT)
+1 0 0
+;
